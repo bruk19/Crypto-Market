178d218756041be7c88e9fb736f977b1bfe85080
bruk19 <brukteshome21@gmail.com>
2022-08-01 22:06:32 +0300

    Add style for navigation design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { TiMicrophone } from 'react-icons/ti';
-import { MdSettings } from 'react-icons/md';
-import logo from '../Assets/logo.png'
+import React from "react";
+import { TiMicrophone } from "react-icons/ti";
+import { MdSettings } from "react-icons/md";
+import logo from "../Assets/logo.png";
 
 const Nav = () => {
   return (
     <>
-    <header>
-    <div className='logo'>
-      <img src={logo} alt='crypto market'></img>
-      <span>Crypto</span>
-    </div>
-    <div className='feature'>
-      <TiMicrophone />
-      <MdSettings />
-    </div>
-    </header>
+      <header>
+        <div className="logo">
+          <img src={logo} alt="crypto market"></img>
+          <span>Crypto</span>
+        </div>
+        <div className="feature">
+          <TiMicrophone color="white" />
+          <MdSettings color="white" />
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
 export default Nav;
