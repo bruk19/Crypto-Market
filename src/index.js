eec97f148d085a739b408c88611fa401a3d4e0e1
bruk19 <brukteshome21@gmail.com>
2022-08-02 16:18:03 +0300

    Add import store to the main page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import App from './App';
-
+import store from './Redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <Router>
         <App />
       </Router>
