27374079c3453334efccade57c34802f52332edd
bruk19 <brukteshome21@gmail.com>
2022-08-02 11:58:59 +0300

    Add configure to the redux

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import CryptoReducer from "./CryptoReducer";
+
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    CryptoReducer,
+    composeEnhancer(applyMiddleware(thunk, logger)),
+);
+
+export default store;
