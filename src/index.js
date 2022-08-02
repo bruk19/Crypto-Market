c05a81e28b25cdd49572bbe89ec0dd7e2902d298
bruk19 <brukteshome21@gmail.com>
2022-08-02 16:17:22 +0300

    Add Provider to the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
