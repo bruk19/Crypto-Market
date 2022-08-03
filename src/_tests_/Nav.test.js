125608cd80fa2d1fc688a97c6574524af786dbb6
bruk19 <brukteshome21@gmail.com>
2022-08-03 14:38:15 +0300

    Add test for navigation and install test render package

---
+++ src/_tests_/Nav.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore';
+import Nav from '../Components/Nav';
+
+it('Test if header render correctly', () => {
+  const TREE = TestRenderer.create(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    </Provider>,
+  );
+  expect(TREE).toMatchSnapshot();
+});
